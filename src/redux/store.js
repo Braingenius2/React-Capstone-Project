22b2d6a7517a284b5fe71c2fdc9674442dab32b4
Braingenius2 <braingenius2@gmail.com>
2023-08-04 23:07:01 +0300

    Fix Eslint errors

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import { configureStore } from '@reduxjs/toolkit';
 import continentsReducer from './continents/continentsSlice';
 import countriesReducer from './countries/countriesSlice';
