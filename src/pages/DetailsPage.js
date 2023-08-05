bfdfece5a0953fdf2907a4309c3a2bb5cef02040
Braingenius2 <braingenius2@gmail.com>
2023-08-05 13:22:16 +0300

    Add Details Page

---
+++ src/pages/DetailsPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Hero from "../components/Hero";
+import CountriesList from "../components/CountriesList";
+
+const DetailsPage = () => (
+  <div>
+    <Hero />
+    <CountriesList />
+  </div>
+);
+
+export default DetailsPage;
