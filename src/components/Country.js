21be0d69108307eb1a11e42acf7bc0bfffe7c151
Braingenius2 <braingenius2@gmail.com>
2023-08-05 13:09:18 +0300

    Add Country component

---
+++ src/components/Country.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Country = ({
+  flag, name, covidCases,
+}) => {
+  const countryName = name.replace(/\s/g, '-');
+
+  return (
+    <a href={`${countryName}`}>
+      <div>
+        <img src={flag} alt={`flag of ${countryName}`} />
+      </div>
+      <div>
+        <h3>{name}</h3>
+        <p >
+          {covidCases}
+          {' '}
+          Covid Cases
+        </p>
+      </div>
+      <div >
+        <div>right arrow in circle here</div>
+      </div>
+    </a>
+  );
+};
+
+CountryItem.propTypes = {
+  flag: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  covidCases: PropTypes.string.isRequired,
+};
+
+export default Country;
