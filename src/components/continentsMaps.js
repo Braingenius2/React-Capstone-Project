a58b0f147517ae4a5e44494fa870b55c84083d5d
Braingenius2 <braingenius2@gmail.com>
2023-08-05 13:17:08 +0300

    Fix eslint errors

--- src/components/continentsMaps.js
+++ src/components/continentsMaps.js
@@ -1,15 +1,15 @@
 import africaMap from '../images/Africa-Map.png';
 import asiaMAp from '../images/asia-map.png';
-import australiaMap from '../images/AustraliaOceania.png'
-import europeMap  from '../images/europe-map.png';
+import australiaMap from '../images/AustraliaOceania.png';
+import europeMap from '../images/europe-map.png';
 import northAmericaMap from '../images/North-america-map.png';
 import southAmericaMap from '../images/south-america-map.png';
 
 const continentMaps = {
-  'Africa': africaMap,
-  'Asia': asiaMAp,
+  Africa: africaMap,
+  Asia: asiaMAp,
   'Australia-Oceania': australiaMap,
-  'Europe': europeMap,
+  Europe: europeMap,
   'North America': northAmericaMap,
   'South America': southAmericaMap,
 };
