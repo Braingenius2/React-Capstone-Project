2571e31d616e1408ee5412942f1e09716de96a72
Braingenius2 <braingenius2@gmail.com>
2023-08-05 13:17:28 +0300

    Add HomePage

---
+++ src/pages/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Hero from '../components/Hero';
+import ContinentsList from '../components/ContinentsList';
+
+const HomePage = () => (
+  <div>
+    <Hero />
+    <ContinentsList />
+  </div>
+);
+
+export default HomePage;
