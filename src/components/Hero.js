1bd6647764a2dbe969012c8f3d629c0bf0cc904e
Braingenius2 <braingenius2@gmail.com>
2023-08-05 13:19:50 +0300

    Add Hero component

---
+++ src/components/Hero.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+const Hero = ({ imageSrc, name, details }) => (
+  <div className="hero">
+    <div>
+      <img src={imageSrc} alt={name} />
+    </div>
+    <div>
+      <h1>{ name }</h1>
+      {
+        details.map(
+          ({ stats, text }) => (
+            <p key={text}>
+              { stats }
+              {' '}
+              { text }
+            </p>
+          ),
+        )
+      }
+    </div>
+  </div>
+);
+
+Hero.defaultProps = {
+  imageSrc: '',
+  name: '',
+  details: [],
+};
+
+Hero.propTypes = {
+  imageSrc: PropTypes.string,
+  name: PropTypes.string,
+  details: PropTypes.arrayOf(PropTypes.shape({
+    stats: PropTypes.string,
+    text: PropTypes.string,
+  })),
+};
+
+export default Hero;
