f0f0b2e6d07c539645ca040442e170650b8e42bf
Braingenius2 <braingenius2@gmail.com>
2023-08-05 13:05:17 +0300

    Add countriesSlice

--- src/redux/countries/countriesSlice.js
+++ src/redux/countries/countriesSlice.js
@@ -1,15 +1,46 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
 
 const initialState = {
   countries: [],
-  isLoading: false,
+  selectedCountry: null,
+  loading: false,
   error: null,
 };
+
+const fetchCountries = createAsyncThunk('countries/fetch', async (_, { rejectWithValue }) => {
+  try {
+    const response = await axios.get('https://disease.sh/v3/covid-19/countries');
+    return response.data;
+  } catch (error) {
+    return rejectWithValue('There was an error fetching countries', error);
+  }
+});
 
 const countriesSlice = createSlice({
   name: 'countries',
   initialState,
-  reducers: [],
+  reducers: {
+    selectCountry: (state, action) => {
+      state.selectedCountry = action.payload;
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchCountries.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(fetchCountries.fulfilled, (state, action) => {
+        state.countries = action.payload;
+        state.loading = false;
+        state.error = null;
+      })
+      .addCase(fetchCountries.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error.message;
+      });
+  },
 });
 
+export const { selectCountry } = countriesSlice.actions;
 export default countriesSlice.reducer;
