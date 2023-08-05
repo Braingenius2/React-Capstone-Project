054ce538e420b74af4621c25b94c878d69615d10
Braingenius2 <braingenius2@gmail.com>
2023-08-05 18:54:55 +0300

    Refactor continentsSlice

--- src/redux/continents/continentsSlice.js
+++ src/redux/continents/continentsSlice.js
@@ -1,6 +1,29 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+import { v4 as uuidv4 } from 'uuid';
+
+const fetchContinents = createAsyncThunk('fetchContinents', async (_, { rejectWithValue }) => {
+  try {
+    const response = await axios.get('https://disease.sh/v3/covid-19/continents');
+    return response.data;
+  } catch (error) {
+    return thunkAPI.rejectWithValue('There was an error fetching all continents', error);
+  }
+});
+
+const fetchAContinent = createAsyncThunk('fetchAContinent', async (continent, { rejectWithValue }) => {
+  try {
+    const response = await axios.get(`${apiEndpoint}/${continent}`);
+    return response.data;
+  } catch (error) {
+    return rejectWithValue('There was an error fetching the continent', error);
+  }
+});
 
 const initialState = {
+  continents: [],
+  selectedContinent: null,
+  totalCases: '',
   isLoading: false,
   error: null,
 };
@@ -8,8 +31,39 @@ const initialState = {
 const continentsSlice = createSlice({
   name: 'continents',
   initialState,
-  reducers: {
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchContinents.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(fetchContinents.fulfilled, (state, action) => {
+        state.isLoading = false;
+        const continents = action.payload;
+        state.continents = continents.map((continent) => (
+          {
+            id: uuidv4(),
+            name: continent.continent,
+            cases: continent.cases.toLocaleString('en-US'),
+            recovered: continent.recovered.toLocaleString('en-US'),
+          }
+        ));
+        state.totalCases = continents.reduce((x, continent) => x + continent.cases, 0).toLocaleString('en-US');
+      })
+      .addCase(fetchContinents.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.payload;
+      })
+      .addCase(fetchAContinent.fulfilled, (state, action) => {
+        const continent = action.payload;
+        state.selectedContinent = {
+          population: continent.population.toLocaleString('en-US'),
+          cases: continent.cases.toLocaleString('en-US'),
+          recovered: continent.recovered.toLocaleString('en-US'),
+        };
+      });
   },
 });
 
+export { fetchContinents, fetchAContinent }; 
 export default continentsSlice.reducer;
