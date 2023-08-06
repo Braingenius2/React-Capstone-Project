93a5158521995bd76ee0ffb97aa5a6edbed0f006
Braingenius2 <braingenius2@gmail.com>
2023-08-06 05:44:50 +0300

    Add CountryDetail

---
+++ src/components/CountryDetail.js
@@ -0,0 +1,19 @@
+import PropTypes from 'prop-types';
+
+const CountryDetail = ({ title, amount }) => (
+  <li>
+    <div>
+      <h3 >{ title }</h3>
+    </div>
+    <div>
+      <p>{ amount }</p>
+    </div>
+  </li>
+);
+
+CountryDetail.propTypes = {
+  title: PropTypes.string.isRequired,
+  amount: PropTypes.string.isRequired,
+};
+
+export default CountryDetail;
